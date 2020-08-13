676e8f16f4ad08af931e1b1ed37553cc5087663f
Nicholai <Nicholai_wp@hotmail.com>
2020-08-13 13:30:42 +0200

    remaining files

---
+++ ShopLibrary/Store.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopLibrary
+{
+   public class Store
+    {
+        public string Name { get; set; }
+        public List<Vendor> Vendors { get; set; }
+        public List<item> Items { get; set; }
+
+        public Store()
+        {
+            Vendors = new List<Vendor>();
+            Items = new List<item>();
+        }
+    }
+}
